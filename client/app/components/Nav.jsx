6c7030ab843bb655a90ac3290f0a4c810d2a1d10
lucas <lucasalves.souza01@gmail.com>
2024-04-13 16:44:53 -0300

    feat: refactor collapse menu button

--- client/app/components/Nav.jsx
+++ client/app/components/Nav.jsx
@@ -1,6 +1,8 @@
 "use client"
 import { useState } from 'react'
 import Image from "next/image";
+import ArrowLeft from './icons/ArrowLeft';
+import ArrowRight from './icons/ArrowRight';
 
 const Nav = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -12,21 +14,25 @@ const Nav = () => {
   return (
       <div className="flex h-screen">          
           {/* Sidebar */}
-          <div className={`bg-gray-800 w-1/4 h-full text-white p-3 ${isSidebarOpen ? '' : 'hidden'}`}>
-              <div className='flex justify-between'>
-                <Image
-                  src="/vercel.svg"
-                  alt="Vercel Logo"
-                  className="dark:invert"
-                  width={100}
-                  height={24}
-                  priority
-                />
+          <div className={`bg-gray-800 h-full text-white p-3 ${isSidebarOpen ? 'w-1/4' : 'w-[50px]'}`}>
+              <div className={`flex justify-center ${isSidebarOpen ? 'md:justify-between' : 'justify-center'}`}>
+                {
+                  isSidebarOpen && 
+                    <Image
+                      src="/vercel.svg"
+                      alt="Vercel Logo"
+                      className="dark:invert hidden md:block"
+                      width={100}
+                      height={24}
+                      priority
+                    />
+                }
                 <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-end"
+                    className="rounded-full"
                     onClick={toggleSidebar}
                 >
-                    {isSidebarOpen ? 'Collapse Sidebar' : 'Expand Sidebar'}
+                   
+                   {isSidebarOpen ? <ArrowLeft /> : <ArrowRight />}
                 </button>
               </div>
               <div>
