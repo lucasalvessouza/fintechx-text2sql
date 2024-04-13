2d1712f2fc766d05173858014dba9a9dff5e234e
lucas <lucasalves.souza01@gmail.com>
2024-04-13 18:31:26 -0300

    feat: add loading to button

--- client/app/page.js
+++ client/app/page.js
@@ -3,12 +3,15 @@ import { useState } from "react";
 import toast from 'react-hot-toast'
 import CopyIcon from "./components/icons/CopyIcon";
 import { generateSql } from "@/hooks/useApi";
+import LoadIcon from "./components/icons/LoadIcon";
 
 export default function Home() {
   const [question, setQuestion] = useState()
   const [sql, setSql] = useState()
+  const [isLoading, setIsLoading] = useState(false)
 
   const submitGenererateSql = () => {
+    setIsLoading(true)
     generateSql({ question })
       .then(response => {
         setSql(response.result.replace('```sql', '').replace('```', ''))
@@ -19,7 +22,7 @@ export default function Home() {
         toast.error('Ocorreu um erro! Tente novamente em alguns minutos', {
           icon: '❌'
         })
-      })
+      }).finally(() => setIsLoading(false))
   }
 
   const copyToClipboard = () => {
@@ -40,7 +43,14 @@ export default function Home() {
           onChange={(e) => setQuestion(e.target.value)}
         />
       </div>
-      <button className="rounded-md bg-blue-500 p-2 text-white font-bold" onClick={submitGenererateSql}>Gerar SQL</button>
+      <button
+        className={`flex flex-row gap-2 justify-center rounded-md bg-blue-500 p-2 text-white font-bold ${isLoading && 'opacity-50'}`}
+        onClick={submitGenererateSql}
+        disabled={isLoading}
+      >
+        <LoadIcon className={isLoading && `w-6 h-6 animate-spin`} />
+        Gerar SQL
+      </button>
       <textarea type="text" className="rounded-md p-3 min-h-[300px] bg-gray-800 border-solid border-2 border-gray-500 text-white" placeholder="SELECT * FROM users WHERE name = 'John'" value={sql}/>
       <div className="flex">
         <button className="flex flex-row gap-2 justify-center rounded-md bg-blue-500 text-white font-bold p-2 w-[100px]" onClick={copyToClipboard}>
